f4c72143639f5df0703a52138db266bbff1d2ec5
Rahul Arulkumaran <rahuldravid313@gmail.com>
2018-05-19 00:57:06 +0530

    Add files via upload

---
+++ options.py
@@ -0,0 +1,54 @@
+from telegram.ext import CommandHandler, Updater
+from telegram import *
+import syntaxdb
+
+def start(bot, update):
+	bot.sendChatAction(chat_id = update.message.chat_id, action = ChatAction.TYPING)
+	
+	bot.sendMessage(chat_id = update.message.chat_id, text = '''
+		Hey %s %s! Welcome to SyntaxDB Bot! Type /help for more information regarding the functionalities of this particular bot.
+	''' %(update.message.from_user.first_name,update.message.from_user.last_name))
+
+def search(bot, update, args):
+	topic = ""
+	i = 0
+	count = 0
+	for arg in args:
+		if(i < (len(args) - 1)):
+			topic += arg + " "
+		else:
+			topic += arg
+		i += 1
+	print("\""+ topic + "\"")
+	syntax = syntaxdb.Syntaxdb(choice = 0, search_query = topic, language = "")
+	content = syntax.getContent()
+	print(content)
+	
+	if(content == ""):
+		error = "Enter a valid search option"
+		bot.sendChatAction(chat_id = update.message.chat_id, action = ChatAction.TYPING)
+		bot.sendMessage(chat_id = update.message.chat_id, text = error)
+	else:
+		for i in range(0,len(content)):
+			count += 1
+			bot.sendChatAction(chat_id = update.message.chat_id, action = ChatAction.TYPING)
+			bot.sendMessage(chat_id = update.message.chat_id, parse_mode=ParseMode.HTML, text = content[i])
+
+	bot.sendChatAction(chat_id = update.message.chat_id, action = ChatAction.TYPING)
+	bot.sendMessage(chat_id = update.message.chat_id, text =  '''
+	There are no related searches to the topic you entered! Make sure your search query was right and try again! Check for spelling mistakes and follow the format in /help
+	''')
+
+
+
+def help(bot, update):
+	bot.sendChatAction(chat_id = update.message.chat_id, action = ChatAction.TYPING)
+	bot.sendMessage(chat_id = update.message.chat_id, text = '''
+		To use this bot, look at the format below!
+		/search <concept_name> <language>
+		Example : Suppose you want to use how to use the concept for in java,
+		/search for in java
+	''')
+
+
+
